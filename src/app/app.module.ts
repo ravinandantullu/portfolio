560a94205b1613fb75fecf9b7cf6956922d2f164
ravinandantullu <ravinandantulluri@gmail.com>
2020-01-28 21:11:34 -0800

    day 2 practice.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResumeComponent } from './resume/resume.component';
+import { FlexBoxComponent } from './practice/flex-box/flex-box.component';
+import { AbsoluteRelativeComponent } from './practice/designPatterns/practice/absolute-relative/absolute-relative.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResumeComponent
+    ResumeComponent,
+    FlexBoxComponent,
+    AbsoluteRelativeComponent
   ],
   imports: [
     BrowserModule,
